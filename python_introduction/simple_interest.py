9876d4909ffa0ed4e089fcd2340b4f43bd204859
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-09-10 15:40:29 +0200

    Changed variable name for exercise

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-simple_interest = (principal * rate * time)
-print("The simple interest is: " + str(simple_interest))
+interest = principal * rate * time
+
+print("The simple interest is: " + str(interest))
