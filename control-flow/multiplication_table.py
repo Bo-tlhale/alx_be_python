bdc0db890425c898d98b675798e299433ee6c895
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-09-18 16:32:29 +0200

    Updated print statement for the multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for digit in range(1, 11):
     result = number * digit
-    print(str(number) + " * " + str(digit) + " = " + str(result))
+    print(f"{number} * {digit} = {result}")
