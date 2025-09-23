04b3b04c4abc3b53ed5d98724e87b5c871340efe
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-09-23 09:50:30 +0200

    Created the operations module to be used in main

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "You can't divide by 0."
-            else:
-                return num1 / num2
+    if operation == "add":
+        return num1 + num2
+    elif operation ==  "subtract":
+        return num1 - num2
+    elif operation ==  "multiply":
+        return num1 * num2
+    elif operation ==  "divide":
+        if num2 == 0:
+            return "You can't divide by 0."
+        else:
+            return num1 / num2
+    else:
+        "Invalid operation"
             
