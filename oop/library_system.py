bde1ddfcd2ff445d36d92fca6ac1bcb1abc12efc
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-10-23 08:38:15 +0200

    Included a magic method in my classes to minimize the length of code in my library class

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,25 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+        
 class EBook(Book):
     def __init__(self, title, author, filesize):
         super().__init__(title, author)
         self.filesize = filesize
-               
+        
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.filesize}KB"
+         
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
         
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library:
     def __init__(self):
         self.books = []
@@ -22,9 +31,4 @@ class Library:
         
     def list_books(self):
         for book in self.books:
-            if book.__class__.__name__ == "EBook":
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.filesize}KB")
-            elif book.__class__.__name__ == "PrintBook":
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
